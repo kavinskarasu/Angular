86727fb36a7e762a30e796a2a6d00ec4e54b3cb1
kavinskarasu <kavinskarasu@gmail.com>
2022-09-23 20:25:05 +0530

    router modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddLoansComponent } from './add-loans/add-loans.component';
+import { LoanTypesComponent } from './loan-types/loan-types.component';
+import { LoansComponent } from './loans/loans.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // we will create all our routes here
+  {
+    path:'loans',
+    component:LoansComponent
+  },{
+    path:'loan-types',
+    component:LoanTypesComponent
+  },
+  {
+    path:'loans/add-loans',
+    component:AddLoansComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoansComponent } from './loans/loans.component';
+import { LoanTypesComponent } from './loan-types/loan-types.component';
+import { AddLoansComponent } from './add-loans/add-loans.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoansComponent,
+    LoanTypesComponent,
+    AddLoansComponent
   ],
   imports: [
     BrowserModule,
